a6a465e09b302fc3311f3c12c90682858e6a0442
Felipe de Moraes Alves <felipe.moraes.alves@hotmail.com>
2020-08-04 16:14:33 -0300

    no message

---
+++ SearchAntiVirus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SearchAntiVirus {
+    class Program {
+        static void Main(string[] args) {
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(registry_key)) {
+                foreach (string subkey_name in key.GetSubKeyNames()) {
+                    using (Microsoft.Win32.RegistryKey subkey = key.OpenSubKey(subkey_name)) {
+                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                    }
+                }
+            }
+
+
+        }
+    }
+}
